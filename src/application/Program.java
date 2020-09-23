884aceac3ae8560e5014c73b70d872bcf998d694
Matheus-Godinho <matheus.brc22@gmail.com>
2020-09-23 02:26:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,10 +13,12 @@ public class Program {
 		
 		Department department;
 		Seller seller;
+		SellerDao sellerDao;
 		
 		department = new Department(1, "Books");
 		seller = new Seller(21, "Bob", "bob@gmail.com", 
 				new Date(), 3000.00, department);
+		sellerDao = DaoFactory.createSellerDao();
 		System.out.printf("%s%n", seller);
 
 	}
